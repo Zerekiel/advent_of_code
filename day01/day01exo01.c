fa8ad882ec735ba4a5ac24d77799b91b6f5d250f
Zerekiel <clement.deprost@epitech.eu>
2023-12-06 17:06:53 +0100

    [DAY 01][EXO 01] complete exo 1

--- day01/day01exo01.c
+++ day01/day01exo01.c
@@ -1,6 +1,46 @@
 #include <stdlib.h>
-# include <string.h>
+#include <string.h>
 #include <stdio.h>
+
+int getValueFromLine(char *line) {
+
+    //premier et denier chiffre de chaque ligne meme si plusieurs chiffres
+    //si 1 seul chiffre on le double
+
+    int length = strlen(line);
+    int i = 0;
+    int firstTime = 0; 
+    char *charResult;
+    char duoResult[2];
+    int result = 0;
+
+    while (i < length) {
+        if (line[i] > 47 &&  line[i] < 58) {
+            if (firstTime == 0) {
+                charResult = malloc(1);
+                firstTime = 1;
+            } else {
+                charResult = realloc(charResult, 1);
+            }
+            strncat(charResult, &line[i], 1);
+        }
+        i++;
+    }
+
+    if (strlen(charResult) == 1) {
+        //on double le chiffre
+        duoResult[0] = charResult[0];
+        duoResult[1] = charResult[0];
+    } else {
+        //on prends le premier et le dernier chiffre
+        duoResult[0] = charResult[0];
+        duoResult[1] = charResult[strlen(charResult) - 1];
+    }
+    result = atoi(duoResult);
+    printf("%d\n", result);
+    free(charResult);
+    return result;
+}
 
 int main(int ac, char **av) {
 
@@ -8,16 +48,19 @@ int main(int ac, char **av) {
     char *line = NULL;
     size_t len = 0;
     ssize_t read;
+    int finalResult = 0;
 
    fp = fopen(av[1], "r");
     if (fp == NULL)
         exit(EXIT_FAILURE);
 
+
    while ((read = getline(&line, &len, fp)) != -1) {
         //printf("Retrieved line of length %zu :\n", read);
-        printf("%s", line);
+        finalResult += getValueFromLine(line);
     }
 
-   free(line);
+    printf("%d", finalResult);
+    free(line);
     exit(EXIT_SUCCESS);
 }
